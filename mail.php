00348d86a8f0cb668520f62447835fa7de7cc727
Jack Terrell <trifeowns@gmail.com>
2019-01-13 12:53:23 -0500

    first commit

---
+++ mail.php
@@ -0,0 +1,21 @@
+<?php
+
+$message = new \stdClass();
+$message->sender = htmlspecialchars($_POST['email']);
+$message->subject = htmlspecialchars($_POST['subject']);
+$message->body = htmlspecialchars($_POST['body']);
+
+$jsonMessage = json_encode($message);
+
+$jsonMessage = wordwrap($jsonMessage, 70);
+
+$mailed = mail("jterrell@jackterrell.org","CONTACT FROM JACKTERRELL.ORG", $jsonMessage);
+
+if(!$mailed) alert("mail sending failed");
+
+echo
+
+header("Location: https://www.jackterrell.org");
+exit();
+
+?>
